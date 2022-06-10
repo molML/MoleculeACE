a0d9c1b306a58c839f840cf39e13ae9af8b318c8
derekvantilborg <dwvantilborg@gmail.com>
2022-06-10 14:05:26 +0200

    Updated to most recent structure

--- Experiments/train_an_included_model.py
+++ Experiments/train_an_included_model.py
@@ -6,24 +6,37 @@ You can find all included datasets here: utils.datasets
 Supported descriptors and ML algorithms can be found here: utils.Descriptors, utils.Algorithms
 """
 
-from MoleculeACE.benchmark import load_data, models, evaluation, utils
+from MoleculeACE.models import RF, SVM, GBM, KNN, MLP, GCN, MPNN, AFP, GAT, CNN, LSTM, Transformer
+from MoleculeACE.benchmark.const import Descriptors, CONFIG_PATH, datasets, WORKING_DIR, RANDOM_SEED
+from MoleculeACE.benchmark.utils import Data, get_config, calc_rmse, calc_cliff_rmse, cross_validate
 import os
+
 
 # Setup some variables
 dataset = 'CHEMBL287_Ki'
-descriptor = utils.Descriptors.ECFP
-algorithm = utils.Algorithms.GBM
-config_file = os.path.join(utils.WORKING_DIR, 'configures', 'benchmark', dataset,
-                           f'{algorithm.value}_{descriptor.value}.yml')
+descriptor = Descriptors.ECFP
+algorithm = GBM
+
+config_path = os.path.join(CONFIG_PATH, 'benchmark', dataset, f"{algorithm.__name__}_{descriptor.name}.yml")
+hyperparameters = get_config(config_path)
+
 
 if __name__ == '__main__':
 
     # Load data
-    data = load_data(dataset, descriptor=descriptor)
+    data = Data(dataset)
+
+    # Featurize SMILES strings with a specific method
+    data(descriptor)
 
     # Train and a model, if config_file = None, hyperparameter optimization is performed
-    model = models.train_model(data, algorithm=algorithm, config_file=config_file)
-    predictions = model.test_predict()
+    model = algorithm(**hyperparameters)
+    model.train(data.x_train, data.y_train)
 
     # Evaluate your model on activity cliff compounds
-    results = evaluation.evaluate(data=data, predictions=predictions)
+    y_hat = model.predict(data.x_test)
+
+    rmse = calc_rmse(data.y_test, y_hat)
+    rmse_cliff = calc_cliff_rmse(y_test_pred=y_hat, y_test=data.y_test, cliff_mols_test=data.cliff_mols_test)
+
+    print(f"RMSE: {rmse:.4f}\nRMSEcliff: {rmse_cliff:.4f}")
