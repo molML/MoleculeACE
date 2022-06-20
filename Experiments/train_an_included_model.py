cd248ccda74a636ce0e151d33a3d027438ff4370
derekvantilborg <dwvantilborg@gmail.com>
2022-06-20 14:13:22 +0200

    Updated example scripts

--- Experiments/train_an_included_model.py
+++ Experiments/train_an_included_model.py
@@ -1,9 +1,7 @@
 """
-With this script you can train one of the 23 included models
+With this script you can train one of the 24 included models
 
-load one of the pre-processed and split datasets that comes with MoleculeACE
-You can find all included datasets here: utils.datasets
-Supported descriptors and ML algorithms can be found here: utils.Descriptors, utils.Algorithms
+load one of the pre-processed datasets that comes with MoleculeACE
 """
 
 from MoleculeACE.models import RF, SVM, GBM, KNN, MLP, GCN, MPNN, AFP, GAT, CNN, LSTM, Transformer
@@ -12,16 +10,16 @@ from MoleculeACE.benchmark.utils import Data, get_config, calc_rmse, calc_cliff_
 import os
 
 
-# Setup some variables
-dataset = 'CHEMBL287_Ki'
-descriptor = Descriptors.ECFP
-algorithm = GBM
+# Define the dataset, descriptor, and algorithm.
+dataset = 'CHEMBL2034_Ki'  # All available datasets are found at MoleculeACE.benchmark.const.datasets
+descriptor = Descriptors.GRAPH  # pick between Descriptors.ECFP, MACCS, PHYSCHEM, WHIM, Graph, SMILES, TOKENS
+algorithm = MPNN  # pick between RF, SVM, GBM, KNN, MLP, GCN, MPNN, AFP, GAT, CNN, LSTM, Transformer
 
 config_path = os.path.join(CONFIG_PATH, 'benchmark', dataset, f"{algorithm.__name__}_{descriptor.name}.yml")
 hyperparameters = get_config(config_path)
 
 
-if __name__ == '__main__':
+def main():
 
     # Load data
     data = Data(dataset)
@@ -29,14 +27,21 @@ if __name__ == '__main__':
     # Featurize SMILES strings with a specific method
     data(descriptor)
 
-    # Train and a model, if config_file = None, hyperparameter optimization is performed
+    # Get the best hyperparameters from a config file (all datasets+models have already been optimized)
+    config_path = os.path.join(CONFIG_PATH, 'benchmark', dataset, f"{algorithm.__name__}_{descriptor.name}.yml")
+    hyperparameters = get_config(config_path)
+
+    # Train and a model
     model = algorithm(**hyperparameters)
     model.train(data.x_train, data.y_train)
-
-    # Evaluate your model on activity cliff compounds
     y_hat = model.predict(data.x_test)
 
+    # Evaluate your model on activity cliff compounds
     rmse = calc_rmse(data.y_test, y_hat)
     rmse_cliff = calc_cliff_rmse(y_test_pred=y_hat, y_test=data.y_test, cliff_mols_test=data.cliff_mols_test)
 
-    print(f"RMSE: {rmse:.4f}\nRMSEcliff: {rmse_cliff:.4f}")
+    print(f"rmse: {rmse}\nrmse_cliff: {rmse_cliff}")
+
+
+if __name__ == '__main__':
+    main()
--- Experiments/use_your_own_data.py
+++ Experiments/use_your_own_data.py
@@ -1,31 +1,45 @@
 """
-With this script you can train one of the 23 included models with your own data
-
-load one of the pre-processed and split datasets that comes with MoleculeACE
-You can find all included datasets here: utils.datasets
-Supported descriptors and ML algorithms can be found here: benchmark.Descriptors, benchmark.Algorithms
+This is an example script showing how to process your own data. You need to provide SMILES and their bioactivity.
 """
 
-from MoleculeACE.benchmark import load_data, models, evaluation, utils, process_data
+from MoleculeACE.models import RF, SVM, GBM, KNN, MLP, GCN, MPNN, AFP, GAT, CNN, LSTM, Transformer
+from MoleculeACE.benchmark.const import Descriptors
+from MoleculeACE.benchmark.data_prep import process_data
+from MoleculeACE.benchmark.utils import Data, calc_rmse, calc_cliff_rmse
 import os
 
 # Setup some variables
-dataset = os.path.join(utils.WORKING_DIR, 'benchmark_data', 'CHEMBL287_Ki.csv')
-descriptor = utils.Descriptors.ECFP
-algorithm = utils.Algorithms.GBM
+dataset = os.path.join(utils.WORKING_DIR, 'benchmark_data', 'raw', 'CHEMBL234_Ki.csv')
+descriptor = Descriptors.ECFP
+algorithm = GBM
 
-if __name__ == '__main__':
 
-    # Process your data data
-    process_data(dataset, smiles_colname='smiles', y_colname='exp_mean [nM]', test_size=0.2,
-                 fold_threshold=10, similarity_threshold=0.9)
+def main():
 
-    # Load data
-    data = load_data(dataset, descriptor=descriptor, tolog10=True)
+    your_own_data = pd.read_csv(dataset)
 
-    # Train and optimize a model. You can also implement your own model here
-    model = models.train_model(data, algorithm=algorithm, config_file=None)
-    predictions = model.test_predict()
+    # Process your data data. You'll have to provide both the SMILES and the corresponding label
+    df = process_data(your_own_data['smiles'], your_own_data['exp_mean [nM]'], in_log10=False, n_clusters=5,
+                      test_size=0.2, similarity=0.9, potency_fold=10, remove_stereo=True)
+
+    # Load processed data into Data class, which manages train/test splits, molecule featurization, and cliff stuff
+    data = Data(df)
+
+    # Featurize SMILES strings with a specific method
+    data(descriptor)
+
+    # Train a model. You can also implement your own model here.
+    model = algorithm()
+    model.train(data.x_train, data.y_train)
+    y_hat = model.predict(data.x_test)
 
     # Evaluate your model on activity cliff compounds
-    results = evaluation.evaluate(data=data, predictions=predictions)
+    rmse = calc_rmse(data.y_test, y_hat)
+    # If you want, you can provide your own cliff molecules -- according to your definition -- as cliff_mols_test
+    rmse_cliff = calc_cliff_rmse(y_test_pred=y_hat, y_test=data.y_test, cliff_mols_test=data.cliff_mols_test)
+
+    print(f"rmse: {rmse}\nrmse_cliff: {rmse_cliff}")
+
+
+if __name__ == '__main__':
+    main()
