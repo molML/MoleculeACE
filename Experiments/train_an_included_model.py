a57e6531f13bf7a462f1cf0429e1bfb548ab988b
derekvantilborg <Haphaistos1>
2022-07-01 10:07:37 +0200

    Added function to easily fetch the optimized hyperparameters

--- Experiments/train_an_included_model.py
+++ Experiments/train_an_included_model.py
@@ -6,17 +6,12 @@ load one of the pre-processed datasets that comes with MoleculeACE
 
 from MoleculeACE.models import RF, SVM, GBM, KNN, MLP, GCN, MPNN, AFP, GAT, CNN, LSTM, Transformer
 from MoleculeACE.benchmark.const import Descriptors, CONFIG_PATH, datasets, WORKING_DIR, RANDOM_SEED
-from MoleculeACE.benchmark.utils import Data, get_config, calc_rmse, calc_cliff_rmse, cross_validate
-import os
-
+from MoleculeACE.benchmark.utils import Data, calc_rmse, calc_cliff_rmse, cross_validate, get_benchmark_config
 
 # Define the dataset, descriptor, and algorithm.
 dataset = 'CHEMBL2034_Ki'  # All available datasets are found at MoleculeACE.benchmark.const.datasets
 descriptor = Descriptors.GRAPH  # pick between Descriptors.ECFP, MACCS, PHYSCHEM, WHIM, Graph, SMILES, TOKENS
 algorithm = MPNN  # pick between RF, SVM, GBM, KNN, MLP, GCN, MPNN, AFP, GAT, CNN, LSTM, Transformer
-
-config_path = os.path.join(CONFIG_PATH, 'benchmark', dataset, f"{algorithm.__name__}_{descriptor.name}.yml")
-hyperparameters = get_config(config_path)
 
 
 def main():
@@ -28,8 +23,7 @@ def main():
     data(descriptor)
 
     # Get the best hyperparameters from a config file (all datasets+models have already been optimized)
-    config_path = os.path.join(CONFIG_PATH, 'benchmark', dataset, f"{algorithm.__name__}_{descriptor.name}.yml")
-    hyperparameters = get_config(config_path)
+    hyperparameters = get_benchmark_config(dataset, descriptor, algorithm)
 
     # Train and a model
     model = algorithm(**hyperparameters)
