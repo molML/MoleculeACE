28739226899633fcb14a6eb4b6356e825c3acf62
derekvantilborg <dwvantilborg@gmail.com>
2022-07-18 09:50:38 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MoleculeACE',
-    version='2.0.0',
+    version='2.0.1',
     packages=['MoleculeACE','MoleculeACE.benchmark',
                                 'MoleculeACE.benchmark.data_fetching',
                             'MoleculeACE.models',
